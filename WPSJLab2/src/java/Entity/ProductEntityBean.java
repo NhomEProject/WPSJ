39163ca8ea101c26fcc5dd428ccc096bad202469
NamBH <vn.apnic@gmail.com>
2015-04-08 19:41:18 +0700

    codeNew

---
+++ WPSJLab2/src/java/Entity/ProductEntityBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author NamIT
+ */
+public class ProductEntityBean {
+    int id;
+    String name;
+    int amount;
+    float price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    
+}
