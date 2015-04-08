490e9ff9d2b23f9c67a80a557b3eb6a1eaf19e0c
NamBH <vn.apnic@gmail.com>
2015-04-08 19:41:52 +0700

    Revert "codeNew"
    This reverts commit 39163ca8ea101c26fcc5dd428ccc096bad202469.

---
+++ src/java/Entity/StudentEntityBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author NamIT
+ */
+public class StudentEntityBean {
+    int id;
+    String name;
+    int age;
+    String adđress;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAdđress() {
+        return adđress;
+    }
+
+    public void setAdđress(String adđress) {
+        this.adđress = adđress;
+    }
+
+   
+}
